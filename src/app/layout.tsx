d9c979b0c47ba6dae62300bade552de7110e7a5f
gmajor07 <gkyando5@gmail.com>
2025-09-03 16:40:45 +0300

    second  commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,44 @@
+/* eslint-disable @next/next/next-script-for-ga */
+import "./globals.css";
+import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
+import { Layout, FixedPlugin } from "@/components";
+import { Navbar, Footer } from "@/components";
+
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "700", "900"],
+  display: "swap",
+});
+
+export const metadata: Metadata = {
+  title: "Developer Portfolio Page",
+  description:
+    "Introducing Developer Portfolio Page, meet an experinced mobile apps developer android & ios .",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <script
+          defer
+          data-site="YOUR_DOMAIN_HERE"
+          src="https://api.nepcha.com/js/nepcha-analytics.js"
+        ></script>
+        <link rel="shortcut icon" href="/favicon.png" type="image/png" />
+      </head>
+      <body className={roboto.className}>
+        <Layout>
+          {children}
+          <FixedPlugin />
+        </Layout>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/projects.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import { ProjectCard } from "@/components";
+import { Typography } from "@material-tailwind/react";
+import { getImagePrefix } from "../../utils/utils";
+
+const PROJECTS = [
+  {
+    img: `${getImagePrefix()}image/w1.svg`,
+    title: "Mobile App Development",
+    desc: "Mobile app designed to help users discover and explore local restaurants and cuisines.",
+  },
+  {
+    img: `${getImagePrefix()}image/n1.svg`,
+    title: "Landing Page Development",
+    desc: "Promotional landing page for a  fitness website Summer Campaign. Form development included.",
+  },
+  {
+    img: `${getImagePrefix()}image/t1.svg`,
+    title: "Mobile App Development",
+    desc: "Mobile app designed to help users discover and explore local restaurants and cuisines.",
+  },
+  {
+    img: `${getImagePrefix()}image/m1.svg`,
+    title: "E-commerce development",
+    desc: "Ecommerce website offering  access to the latest and greatest gadgets and accessories.",
+  },
+  {
+    img: `${getImagePrefix()}image/d1.svg`,
+    title: "Mobile App Development",
+    desc: "Mobile app designed to help users discover and explore local restaurants and cuisines.",
+  },
+  {
+    img: `${getImagePrefix()}image/n2.svg`,
+    title: "Landing Page Development",
+    desc: "Promotional landing page for a  fitness website Summer Campaign. Form development included.",
+  },
+  {
+    img: `${getImagePrefix()}image/l3.svg`,
+    title: "Mobile App Development",
+    desc: "Mobile app designed to help users discover and explore local restaurants and cuisines.",
+  },
+  {
+    img: `${getImagePrefix()}image/l4.svg`,
+    title: "E-commerce development",
+    desc: "Ecommerce website offering  access to the latest and greatest gadgets and accessories.",
+  },
+];
+
+export function Projects() {
+  return (
+    <section className="py-28 px-8">
+      <div className="container mx-auto mb-20 text-center">
+        <Typography variant="h2" color="blue-gray" className="mb-4">
+          My Projects
+        </Typography>
+        <Typography
+          variant="lead"
+          className="mx-auto w-full px-4 font-normal !text-gray-500 lg:w-6/12"
+        >
+          Whether you have a mobile app idea that needs to come to life or a
+          website that requires a facelift, I&apos;m here to turn your digital
+          dreams into reality.
+        </Typography>
+      </div>
+      <div className="container mx-auto grid grid-cols-1 gap-x-10 gap-y-20 md:grid-cols-2 xl:grid-cols-4">
+        {PROJECTS.map((props, idx) => (
+          <ProjectCard key={idx} {...props} />
+        ))}
+      </div>
+    </section>
+  );
+}
+
+export default Projects;
